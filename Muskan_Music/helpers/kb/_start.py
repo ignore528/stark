5e3450ea8799752aef251e87540063c144f2c6fd
Stark773 <akgameing452@gmail.com>
2026-08-02 11:38:04 +0545

    Fix syntax error in success_button call

--- Muskan_Music/helpers/kb/_start.py
+++ Muskan_Music/helpers/kb/_start.py
@@ -29,7 +29,7 @@ def private_panel(_):
         ],
         [
             danger_button(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            success_button(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            success_button(text=_["S_B_2"], url=config.SUPPORT_CHAT",
             icon_custom_emoji_id=5443038326535759644),
         ],
     ]
