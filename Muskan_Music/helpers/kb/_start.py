07b8d33e80cdd6388d9801cd11387852dd5d45a5
Stark773 <akgameing452@gmail.com>
2026-08-11 00:35:26 +0545

    Update _start.py

--- Muskan_Music/helpers/kb/_start.py
+++ Muskan_Music/helpers/kb/_start.py
@@ -9,7 +9,7 @@ def start_panel(_):
     buttons = [
         [
             primary_button(
-                text=_["SO_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["SO_B_1"], url=f"https://t.me/{app.username}?startgroup=true" icon_custom_emoji_id=5258362837411045098),
             ),
             success_button(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
@@ -21,17 +21,17 @@ def private_panel(_):
         [
             primary_button(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=true", icon_custom_emoji_id=5258362837411045098),
             )
         ],
         [
-            success_button(text=_["S_B_4"], callback_data="settings_back_helper"),
+            success_button(text=_["S_B_4"], callback_data="settings_back_helper" icon_custom_emoji_id=5314504236132747481),
         ],
         [
-            danger_button(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, icon_custom_emoji_id=5375338737028841420),
+            danger_button(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, icon_custom_emoji_id=5424818078833715060),
             success_button(text=_["S_B_2"], url=config.SUPPORT_CHAT, icon_custom_emoji_id=5443038326535759644),
         ],
     ]
     if config.SOURCE and config.SOURCE.startswith("http"):
-        buttons.append([primary_button(text=_["S_B_7"], url=config.SOURCE)])
+        buttons.append([primary_button(text=_["S_B_7"], url=config.SOURCE" icon_custom_emoji_id=5253742260054409879),
     return buttons
