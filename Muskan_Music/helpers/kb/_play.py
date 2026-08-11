36e5809b2b6fa0c09c1209cf1b7802fe08b37d45
Stark773 <akgameing452@gmail.com>
2026-08-11 10:20:56 +0545

    Fix icon_custom_emoji_id for ADMIN Skip button

--- Muskan_Music/helpers/kb/_play.py
+++ Muskan_Music/helpers/kb/_play.py
@@ -59,7 +59,7 @@ def stream_markup_timer(_, chat_id, played, dur, autoplay_on: bool = False):
             success_button(text="", callback_data=f"ADMIN Resume|{chat_id}", icon_custom_emoji_id=5996789164383669947),
             danger_button(text="", callback_data=f"ADMIN Pause|{chat_id}", icon_custom_emoji_id=5900130736408629854),
             primary_button(text="", callback_data=f"ADMIN Replay|{chat_id}", icon_custom_emoji_id=5877680341057015789),
-            success_button(text="", callback_data=f"ADMIN Skip|{chat_id}", icon_custom_emoji_id=5449430500592604057),
+            success_button(text="", callback_data=f"ADMIN Skip|{chat_id}", icon_custom_emoji_id=5877680341057015789),
             danger_button(text="", callback_data=f"ADMIN Stop|{chat_id}", icon_custom_emoji_id=5778527486270770928),
         ],
         [
