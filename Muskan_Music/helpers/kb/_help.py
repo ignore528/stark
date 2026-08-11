00d1fd5b33b56076405a16781c4c2f5cd4464e01
Stark773 <akgameing452@gmail.com>
2026-08-11 14:00:44 +0545

    Fix indentation for primary_button in _help.py

--- Muskan_Music/helpers/kb/_help.py
+++ Muskan_Music/helpers/kb/_help.py
@@ -47,7 +47,7 @@ def help_back_markup(_):
             [
                 primary_button(
                     text=_["BACK_BUTTON"],
-                    callback_data="settings_back_helper",
+                    callback_data="settings_back_helper", icon_custom_emoji_id=5816895683256390576),
                 ),
             ]
         ]
