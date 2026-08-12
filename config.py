fdedaccf819e5e921c248e91be508c7c88d31e73
Stark773 <akgameing452@gmail.com>
2026-08-13 00:46:28 +0545

    Update PLAYLIST_IMG_URL to new image link

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://litter.catbox.moe/a9oxfp.jpg"
 )
-PLAYLIST_IMG_URL = "https://litter.catbox.moe/a9oxfp.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/c832e84cd991c865c7e4f.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
